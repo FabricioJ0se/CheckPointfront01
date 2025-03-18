b07d2374ad17030b6fdf85466d1f57f42fda639a
FabricioJ0se <fabriciojosedasilva06@gmail.com>
2025-03-18 01:27:38 -0300

    Nome do Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { carrosData } from "@/data/carrosData"; 
+import Link from "next/link";
 import "./globals.css";
 
 export default function Home() {
@@ -7,8 +9,22 @@ export default function Home() {
     <>
       <Header />
       <div className="text-center home-container"> 
-        <h1 className="text-3xl font-semibold mb-2">Concessionaria Fiap</h1>
+        <h1 className="text-3xl font-semibold mb-2">Concessionária Fiap</h1>
         <p className="text-lg mb-4">O local perfeito para escolher seu Carro</p>
+        <div className="carros-list grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {carrosData.map((carro) => (
+            <div key={carro.id} className="carro-item bg-white rounded-lg shadow-lg p-4">
+              <img
+                src={`/img/${carro.img}`} 
+                alt={carro.modelo}
+                className="w-full h-40 object-cover rounded-lg"
+              />
+              <h3 className="mt-3 text-lg font-semibold">{carro.modelo}</h3>
+              <p className="text-sm text-gray-500">{carro.preco}</p>
+              <Link href={`/detalhesCarro/${carro.id}`} className="text-blue-500">Ver detalhes</Link>
+            </div>
+          ))}
+        </div>
       </div>
       <Footer />
     </>
